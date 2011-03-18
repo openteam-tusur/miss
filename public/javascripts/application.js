c99951eae302a6172c34808ee6a93158c350a58f
Evgeny Lapin <lev@openteam.ru>
2011-03-18 15:04:43 +0600

    #6976. GUI: customize datepicker

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,14 @@ $(function() {
 
   $(".focus_first:first").focus();
 
-  $("form.formtastic li.date input").datepicker();
+  $("form.formtastic li.date input").attr("autocomplete", "off").datepicker({
+    changeMonth: true,
+    changeYear: true,
+    showOn: "button",
+    buttonImage: "/images/calendar.png",
+    buttonImageOnly: true,
+    buttonText: "Выбрать"
+  });
 
   $(".sortable").sortable({
     axis: "y",
