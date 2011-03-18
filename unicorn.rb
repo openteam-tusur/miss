584f4769b07240fac452c23b217d73473dd97d42
Egor Nazarkin <nimnull@gmail.com>
2011-03-18 15:10:01 +0600

    unicorn workaround fixes

--- unicorn.rb
+++ unicorn.rb
@@ -1,6 +1,5 @@
-app_name = "miss-tusur"
 worker_processes 2
-working_directory "/srv/#{app_name}/"
+working_directory "/srv/miss-tusur/"
 
 # This loads the application in the master process before forking
 # # worker processes
@@ -14,11 +13,11 @@ timeout 300
 # # We will point the upstream Nginx module to this socket later on
 listen "0.0.0.0:8083", :backlog => 64
 #
-pid "/var/run/unicorn-#{app_name}.pid"
+pid "/var/run/unicorn-miss-tusur.pid"
 #
 # # Set the path of the log files inside the log folder of the testapp
-stderr_path "/var/log/unicorn/#{app_name}.stderr.log"
-stdout_path "/var/log/unicorn/#{app_name}.stdout.log"
+stderr_path "/var/log/unicorn/miss-tusur.stderr.log"
+stdout_path "/var/log/unicorn/miss-tusur.stdout.log"
 #
 before_fork do |server, worker|
 # # This option works in together with preload_app true setting
