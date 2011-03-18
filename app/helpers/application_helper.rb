8b219808f0739f1f7b77b3988decfeb1ecbb9389
Evgeny Lapin <lev@openteam.ru>
2011-03-18 14:03:00 +0600

    #6975. разборки с титлями

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def title_locale
+    title(t("title.#{params[:controller]}.#{params[:action]}"), true)
+    t("title.#{params[:controller]}.#{params[:action]}")
+  end
+
+  def title(page_title, show_title = true)
+    content_for(:title) { page_title.to_s }
+    @show_title = show_title
+  end
+
+  def show_title?
+    @show_title
+  end
+
 end
+
