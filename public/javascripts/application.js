c6d342f3e089a62d5ba3cfbcdade67bc233a3e1f
Evgeny Lapin <lev@openteam.ru>
2011-03-21 14:11:44 +0600

    js: auto generate slug

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,52 @@
+function create_slug() {
+  var ru_en = [
+    ["а", "a"],
+    ["б", "b"],
+    ["в", "v"],
+    ["г", "g"],
+    ["д", "d"],
+    ["е", "e"],
+    ["ё", "yo"],
+    ["ж", "zh"],
+    ["з", "z"],
+    ["и", "i"],
+    ["й", "y"],
+    ["к", "k"],
+    ["л", "l"],
+    ["м", "m"],
+    ["н", "n"],
+    ["о", "o"],
+    ["п", "p"],
+    ["р", "r"],
+    ["с", "s"],
+    ["т", "t"],
+    ["у", "u"],
+    ["ф", "f"],
+    ["х", "h"],
+    ["ц", "c"],
+    ["ч", "ch"],
+    ["ш", "sh"],
+    ["щ", "shj"],
+    ["ъ", "j"],
+    ["ы", "y"],
+    ["ь", "j"],
+    ["э", "ye"],
+    ["ю", "ju"],
+    ["я", "ja"]
+  ];
+  if ($("#member_slug").val() == "") {
+    $('#member_name').blur(function() {
+      var source = $(this).val().toLowerCase().replace(" ", "-");
+      for (var i=0; i < ru_en.length; i++) {
+        var elem = ru_en[i];
+        source = source.replace(new RegExp(elem[0], "g"), elem[1]);
+      };
+      $("#member_slug").val(source);
+      console.log($("#member_slug").val());
+    });
+  };
+};
+
 $(function() {
 
   $(".focus_first:first").focus();
@@ -24,6 +73,8 @@ $(function() {
     close: "закрыть",
     current:	"{current} из {total}"
   });
+
+  create_slug();
 
 });
 
